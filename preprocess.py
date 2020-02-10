c7fe6e17b111e579ea5d8c0b4ceef6202c6d0dbb
Tailgunnerii <eero.laaksonen@gmail.com>
2020-02-09 16:50:24 -0800

    fixing input path

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 from shutil import copyfile
 
 #Just copying my inputs to outputs
-src="/valohai/inputs/winequalityN.csv"
+src="/valohai/inputs/data/winequalityN.csv"
 dst="/valohai/outputs/winequalityN.csv"
 copyfile(src, dst)
